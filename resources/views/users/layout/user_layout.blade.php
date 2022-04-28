c30e98fa5f362fb0142b115f7b83d95df32b5e86
emmanuel-ochii <emmanuelochubili@gmail.com>
2022-04-28 10:18:22 +0100

    Added BingHR logo

--- resources/views/users/layout/user_layout.blade.php
+++ resources/views/users/layout/user_layout.blade.php
@@ -246,8 +246,8 @@ control > ::placeholder{
         <div class="main-sidebar" style="background-color:#0395FF">
             <div class="sidebar-brand">
                 <a href="/">
-                    <img class="light-image" src="img/logos/logo/logo.svg" alt="">
-                    <img class="dark-image" src="img/logos/logo/logo-light.svg" alt="">
+                    <img class="light-image" src="{{asset('img/logos/binghr.svg')}}" alt="">
+                    <img class="dark-image" src="{{asset('img/logos/binghr.svg')}}" alt="">
                 </a>
             </div>
             <div class="sidebar-inner">
@@ -353,101 +353,7 @@ control > ::placeholder{
                 </ul>
             </div>
         </div>
-        <!--Search panel-->
-        <div id="search-panel" class="right-panel-wrapper is-search is-left">
-            <div class="panel-overlay"></div>
 
-            <div class="right-panel">
-                <div class="right-panel-head">
-                    <img class="light-image" src="img/logos/logo/logo.svg" alt="" />
-                    <img class="dark-image" src="img/logos/logo/logo-light.svg" alt="" />
-                    <a class="close-panel">
-                        <i data-feather="chevron-left"></i>
-                    </a>
-                </div>
-                <div class="right-panel-body has-slimscroll">
-                    <div class="field">
-                        <div class="control has-icon">
-                            <input type="text" class="input is-rounded search-input" placeholder="Search...">
-                            <div class="form-icon">
-                                <i data-feather="search"></i>
-                            </div>
-                            <div class="search-results has-slimscroll"></div>
-                        </div>
-                    </div>
-
-                    <div class="recent">
-                        <h4>Recently viewed</h4>
-                        <div class="recent-block">
-                            <a class="media-flex-center">
-                                <div class="h-icon is-info is-rounded is-small">
-                                    <i data-feather="chrome"></i>
-                                </div>
-                                <div class="flex-meta">
-                                    <span>Browser Support</span>
-                                    <span>Blog post</span>
-                                </div>
-                            </a>
-                            <a class="media-flex-center">
-                                <div class="h-icon is-orange is-rounded is-small">
-                                    <i data-feather="tv"></i>
-                                </div>
-                                <div class="flex-meta">
-                                    <span>Twitch API</span>
-                                    <span>Blog post</span>
-                                </div>
-                            </a>
-                            <a class="media-flex-center">
-                                <div class="h-icon is-green is-rounded is-small">
-                                    <i data-feather="twitter"></i>
-                                </div>
-                                <div class="flex-meta">
-                                    <span>Twitter Auth</span>
-                                    <span>Blog post</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="recent">
-                        <h4>Recent Members</h4>
-                        <div class="recent-block">
-                            <a class="media-flex-center">
-                                <div class="h-avatar is-small">
-                                    <img class="avatar" src="https://via.placeholder.com/150x150"
-                                        data-demo-src="img/avatars/photos/7.jpg" alt="" data-user-popover="0">
-                                </div>
-                                <div class="flex-meta">
-                                    <span>Alice C.</span>
-                                    <span>Software Engineer</span>
-                                </div>
-                            </a>
-                            <a class="media-flex-center">
-                                <div class="h-avatar is-small">
-                                    <img class="avatar" src="https://via.placeholder.com/150x150"
-                                        data-demo-src="img/avatars/photos/13.jpg" alt="" data-user-popover="6">
-                                </div>
-                                <div class="flex-meta">
-                                    <span>Tara S.</span>
-                                    <span>UI/UX Designer</span>
-                                </div>
-                            </a>
-                            <a class="media-flex-center">
-                                <div class="h-avatar is-small">
-                                    <img class="avatar" src="https://via.placeholder.com/150x150"
-                                        data-demo-src="img/avatars/photos/22.jpg" alt="" data-user-popover="5">
-                                </div>
-                                <div class="flex-meta">
-                                    <span>Jimmy H.</span>
-                                    <span>Project Manager</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
         <!--Page body-->
         <!-- sidebar with contents -->
         <div id="home-sidebar" class="sidebar-panel is-generic">
@@ -1390,7 +1296,7 @@ control > ::placeholder{
                                     <span></span>
                                 </label>
                             </div>
-                            
+
                             <div class="dropdown dropdown-trigger">
                                 <div class="is-trigger">
                                     <button class="button" aria-haspopup="true"
